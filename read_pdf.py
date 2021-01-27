706545113492d31f638b5910f45c524c4cfa4836
Andy Block <ablock91@gmail.com>
2021-01-26 23:03:45 -0500

    added drop_cols input to read_pdf

--- read_pdf.py
+++ read_pdf.py
@@ -8,9 +8,10 @@
 #
 # this function takes the data from tables in pdf files and returns that data as a pandas dataframe
 #
-# the two inputs are:
+# the three inputs are:
 #   - the filepath of the given file that you want to grab the data from
 #   - the numbers of the pages where you want to collect the data from
+#   - any columns the user would like to drop, which helps with concat performance
 #
 # the function iterates thru the pages where the data exists and concats the data together
 #
@@ -21,12 +22,16 @@
 import tabula
 import pandas as pd
     
-def read_pdf(filepath, page_nums):
+def read_pdf(filepath, page_nums, drop_cols=None):
         
     df = pd.DataFrame()
     
     for i in page_nums:
         pdf_table = tabula.read_pdf(filepath, pages=i)
+
+        if drop_cols not None:
+        	pdf_table.drop(columns=drop_cols, inplace=True)
+        
         df = pd.concat([df, pdf_table], ignore_index=True)
     
     return df
